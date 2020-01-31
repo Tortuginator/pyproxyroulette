1c12b88d4fa79e726d7d416b8670afe454c8a9ad
Felix <felix.friedberger@rwth-aachen.de>
2020-01-31 23:21:45 +0100

    Bugfix

--- pyproxyroulette/__init__.py
+++ pyproxyroulette/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.4.7'
+__version__ = '0.4.8'
 
 from .app import ProxyRoulette
 from .defaults import defaults
--- pyproxyroulette/pool.py
+++ pyproxyroulette/pool.py
@@ -104,7 +104,7 @@ class ProxyPool:
                 requests.exceptions.ReadTimeout,
                 requests.exceptions.ChunkedEncodingError,
                 requests.exceptions.ConnectionError,
-                requests.exceptions.ConnectionResetError,
+                ConnectionResetError,
                 requests.exceptions.TooManyRedirects):
             proxy.report_check_failed()
             return False
